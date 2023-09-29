23724e618932709a31d17be65869e040f070b4ed
sukmanurr <sukmanurr@gmail.com>
2023-09-30 01:34:00 +0700

    update page product2 html,css,js

--- JS/product2.js
+++ JS/product2.js
@@ -1,58 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".overlay .closeOverlay").addEventListener("click", function () {
+      document.querySelector(".overlay").classList.remove("active");
+  });
+});
+
+function popup(product){
+  document.querySelector(".overlay").classList.add("active")
+  const productJSON = JSON.parse(product)
+  // console.log(productJSON)
+
+  document.querySelector("#gambar-produk1").src = productJSON.listImage[0]
+  document.querySelector("#gambar-produk2").src = productJSON.listImage[1]
+  document.querySelector("#gambar-produk3").src = productJSON.listImage[2]
+
+  document.querySelector("#nama-produk").innerHTML = productJSON.name
+  document.querySelector("#harga-produk").innerHTML = productJSON.price
+}
+
+function escapeHtml(unsafe)
+{
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+// Shorthand for $( document ).ready()
+$(function() {
+  console.log( "ready!" );
+
+  var urlParams = new URLSearchParams(window.location.search);
+  const getCategory = urlParams.get('category') ? urlParams.get('category') : 'man'
+
+  // if (urlParams.get('category')) {
+  //   getCategory = urlParams.get('category')
+  // }else {
+  //   getCategory = 'man'
+  // }
+
   let listContainer = document.querySelector(".container2");
   let list = document.querySelector(".list");
   let listCard = document.querySelector(".listCard");
   let body = document.querySelector("body");
 
-  let products = [
-    {
-      id: 1,
-      name: "Arjuna Batik Shirt",
-      image: "Batik Pria/Arjuna Batik Shirt.jpg",
-      price: 120000,
-    },
-    {
-      id: 2,
-      name: "Black Shirt with Chestnut Splash",
-      image: "Batik Pria/Black Shirt with Chestnut Splash.jpg",
-      price: 145000,
-    },
-    {
-      id: 3,
-      name: "Sunshine Elegnace Batik Shirt",
-      image: "Batik Pria/Sunshine Elegnace Batik Shirt.jpg",
-      price: 195000,
-    },
-    {
-      id: 4,
-      name: "Lanange Silver Mist Shirt",
-      image: "Batik Pria/Lanange Silver Mist Shirt 1.jpg",
-      price: 180000,
-    },
-    {
-      id: 5,
-      name: "Lanange Tropical Sunrise Shirt",
-      image: "Batik Pria/Lanange Tropical Sunrise Shirt3.jpg",
-      price: 185000,
-    },
-    {
-      id: 6,
-      name: "Mocha Noir Shirt",
-      image: "Batik Pria/Mocha Noir Shirt.jpg",
-      price: 185000,
-    },
-  ];
   let listCards = [];
-  function initApp() {
+  function initApp(products) {
+    console.log('products', products)
     products.forEach((value, key) => {
       let newDiv = document.createElement("div");
-      newDiv.classList.add("item");
+      newDiv.classList.add("item")
       newDiv.innerHTML = `
-            <img src="../Assets/${value.image}">
+            <img id="klik" src="../Assets/${value.image}" onclick="popup('${escapeHtml(JSON.stringify(value))}')">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>`;
       list.appendChild(newDiv);
     });
   }
-  initApp();
+    
+  // ga digunakan kalau sudah pakai api
+  const products = getCategory === 'man' ? productsMan : productsWoman
+  initApp(products); 
+  // ga digunakan kalau sudah pakai api
+
+  // $.get(`https://merdeka.free.beeceptor.com/products?category=${getCategory}`, function(response){ 
+  // $.get(`/api/products?category=${getCategory}`, function(response){ 
+  //       initApp(response);
+  // });
 });
