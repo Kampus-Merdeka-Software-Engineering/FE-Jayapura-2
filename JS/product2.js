ac2593f53a6d4ad4880f8c934b2a08d98ee5989c
Brian Khaw <briankhaw125@gmail.com>
2023-10-02 23:09:57 +0700

    Update Product2 JS

--- JS/product2.js
+++ JS/product2.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".overlay .closeOverlay").addEventListener("click", function () {
+  document
+    .querySelector(".overlay .closeOverlay")
+    .addEventListener("click", function () {
       document.querySelector(".overlay").classList.remove("active");
-  });
+    });
 });
 
-function popup(product){
-  document.querySelector(".overlay").classList.add("active")
-  const productJSON = JSON.parse(product)
+function popup(product) {
+  document.querySelector(".overlay").classList.add("active");
+  const productJSON = JSON.parse(product);
   // console.log(productJSON)
 
-  document.querySelector("#gambar-produk1").src = productJSON.listImage[0]
-  document.querySelector("#gambar-produk2").src = productJSON.listImage[1]
-  document.querySelector("#gambar-produk3").src = productJSON.listImage[2]
+  document.querySelector("#gambar-produk1").src = productJSON.listImage[0];
+  document.querySelector("#gambar-produk2").src = productJSON.listImage[1];
+  document.querySelector("#gambar-produk3").src = productJSON.listImage[2];
 
-  document.querySelector("#nama-produk").innerHTML = productJSON.name
-  document.querySelector("#harga-produk").innerHTML = productJSON.price
+  document.querySelector("#nama-produk").innerHTML = productJSON.name;
+  document.querySelector("#harga-produk").innerHTML = productJSON.price;
 }
 
-function escapeHtml(unsafe)
-{
+function escapeHtml(unsafe) {
   return unsafe
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
@@ -28,11 +29,13 @@ function escapeHtml(unsafe)
 }
 
 // Shorthand for $( document ).ready()
-$(function() {
-  console.log( "ready!" );
+$(function () {
+  console.log("ready!");
 
   var urlParams = new URLSearchParams(window.location.search);
-  const getCategory = urlParams.get('category') ? urlParams.get('category') : 'man'
+  const getCategory = urlParams.get("category")
+    ? urlParams.get("category")
+    : "man";
 
   // if (urlParams.get('category')) {
   //   getCategory = urlParams.get('category')
@@ -47,25 +50,52 @@ $(function() {
 
   let listCards = [];
   function initApp(products) {
-    console.log('products', products)
+    console.log("products", products);
     products.forEach((value, key) => {
       let newDiv = document.createElement("div");
-      newDiv.classList.add("item")
+      newDiv.classList.add("item");
       newDiv.innerHTML = `
-            <img id="klik" src="../Assets/${value.image}" onclick="popup('${escapeHtml(JSON.stringify(value))}')">
+            <img id="klik" src="../Assets/${
+              value.image
+            }" onclick="popup('${escapeHtml(JSON.stringify(value))}')">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>`;
       list.appendChild(newDiv);
     });
   }
-    
+
   // ga digunakan kalau sudah pakai api
   const products = getCategory === 'man' ? productsMan : productsWoman
-  initApp(products); 
+  // function getProducts() {
+  //   fetch("http://localhost:3333/products", {
+  //     method: 'GET',
+  //     mode: 'no-cors',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     }
+  //   })
+  //     .then(function (response) {
+  //       if (response.ok) {
+  //         return response.json();
+        
+  //       }
+  //       return Promise.reject(response);
+  //     })
+  //     .then(function(data){
+  //       console.log(data.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+
+  // }
+
+  // getProducts();
+   initApp(products);
   // ga digunakan kalau sudah pakai api
 
-  // $.get(`https://merdeka.free.beeceptor.com/products?category=${getCategory}`, function(response){ 
-  // $.get(`/api/products?category=${getCategory}`, function(response){ 
+  // $.get(`https://merdeka.free.beeceptor.com/products?category=${getCategory}`, function(response){
+  // $.get(`/api/products?category=${getCategory}`, function(response){
   //       initApp(response);
   // });
 });
