d9a6eb785feaf0521de00c74a471f777ec2ee4a7
ImamGozali <imamgozali.b@gmail.com>
2023-10-10 10:24:51 +0700

    update html javascript

--- JS/session.js
+++ JS/session.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Kode JavaScript Anda di sini
 
-    console.log('Script is running.');
+    console.log('Script is running....');
     // Check if the user is logged in (you can use your own logic to determine this)
     const isLoggedIn = localStorage.getItem('isLoggedIn');
 
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const contactLink = document.getElementById('contact-link');
     const privacyPolicyLink = document.getElementById('privacy-policy-link');
     const termsOfServiceLink = document.getElementById('terms-of-service-link');
+    const dropdownCart = document.getElementById('dropdown-cart');
+    const dropdownCartHome = document.getElementById('dropdown-cart-home');
+    const carouselCart = document.getElementById('carousel-cart');
+    const dropdownCarouselCart = document.getElementById('dropdown-carousel-cart');
 
     // Add a click event listener for cartLink
     if (cartLink) {
@@ -78,6 +82,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (termsOfServiceLink) {
         termsOfServiceLink.addEventListener('click', function () {
+            if (isLoggedIn) {
+                console.log('Redirecting to cart.html');
+                window.location.href = 'cart.html'; // Redirect to cart.html if logged in
+            } else {
+                console.log('Redirecting to Login2.html');
+                window.location.href = 'Login2.html'; // Redirect to Login2.html if not logged in
+            }
+        });
+    }
+
+    if (dropdownCart) {
+        dropdownCart.addEventListener('click', function () {
+            if (isLoggedIn) {
+                console.log('Redirecting to cart.html');
+                window.location.href = 'cart.html'; // Redirect to cart.html if logged in
+            } else {
+                console.log('Redirecting to Login2.html');
+                window.location.href = 'Login2.html'; // Redirect to Login2.html if not logged in
+            }
+        });
+    }
+
+    if (dropdownCartHome) {
+        dropdownCartHome.addEventListener('click', function () {
+            if (isLoggedIn) {
+                console.log('Redirecting to cart.html');
+                window.location.href = './HTML/cart.html'; // Redirect to cart.html if logged in
+            } else {
+                console.log('Redirecting to Login2.html');
+                window.location.href = '/HTML/Login2.html'; // Redirect to Login2.html if not logged in
+            }
+        });
+    }
+
+    if (carouselCart) {
+        carouselCart.addEventListener('click', function () {
+            if (isLoggedIn) {
+                console.log('Redirecting to cart.html');
+                window.location.href = 'cart.html'; // Redirect to cart.html if logged in
+            } else {
+                console.log('Redirecting to Login2.html');
+                window.location.href = 'Login2.html'; // Redirect to Login2.html if not logged in
+            }
+        });
+    }
+
+
+    if (dropdownCarouselCart) {
+        dropdownCarouselCart.addEventListener('click', function () {
             if (isLoggedIn) {
                 console.log('Redirecting to cart.html');
                 window.location.href = 'cart.html'; // Redirect to cart.html if logged in
